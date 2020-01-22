7d32d51f12da56e0bdf7e19f1f7158620156f2a9
JurojinKun <ccommunay@gmail.com>
2020-01-22 19:59:03 +0100

    Préparation de la solution

---
+++ src/Projet.Dotnet.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet.Dotnet.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
